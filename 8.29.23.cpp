1b16bdf9ddf45d8626d7da03cb3dd28a01e8fb90
Megan <mko5258@psu.edu>
2023-08-31 17:45:07 -0400

    Add Quiz 1 Files

---
+++ 8.29.23.cpp
@@ -0,0 +1,12 @@
+﻿// 8.29.23.cpp : Defines the entry point for the application.
+//
+
+#include "8.29.23.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Megan O'Brien" << endl;
+	return 0;
+}
